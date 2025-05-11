45905567540296dcf5f129326f612dc689cb05d7
jck <jckleinbourg@gmail.com>
2025-05-11 16:34:19 +0200

    💄 Change typography to 'Inter'

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
